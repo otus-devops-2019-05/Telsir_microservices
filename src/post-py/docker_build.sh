2affccb63cb9c4b3d4c86d5d6a27043f1bcb3709
telsir <justtelsir@gmail.com>
2019-09-10 20:22:34 +0300

    fix blanc line

--- src/post-py/docker_build.sh
+++ src/post-py/docker_build.sh
@@ -3,4 +3,4 @@
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
 
-sudo docker build -t telsir/post:0.0.2 .
+sudo docker build -t $USER_NAME/post:0.0.2 .
